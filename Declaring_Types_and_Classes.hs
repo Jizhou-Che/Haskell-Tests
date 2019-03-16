5f364ee5818226ebd12344801b2d0c75adca993c
Jizhou Che <che_mail@icloud.com>
2019-03-16 20:16:01 +0800

    Quick revision.

--- Declaring_Types_and_Classes.hs
+++ Declaring_Types_and_Classes.hs
@@ -1,8 +1,8 @@
 data Shape = Circle Float | Rect Float Float
 
 instance Show Shape where
-    show (Circle r) = "Circle " ++ show r
-    show (Rect x y) = "Rectangle " ++ show x ++ " " ++ show y
+  show (Circle r) = "Circle " ++ show r
+  show (Rect x y) = "Rectangle " ++ show x ++ " " ++ show y
 
 square :: Float -> Shape
 square x = Rect x x
@@ -10,3 +10,4 @@ square x = Rect x x
 area :: Shape -> Float
 area (Circle r) = pi * r ^ 2
 area (Rect x y) = x * y
+
---
+++ First_Steps.hs
@@ -0,0 +1,12 @@
+myLast1 :: [a] -> a
+myLast1 xs = head (drop (length xs - 1) xs)
+
+myLast2 :: [a] -> a
+myLast2 xs = xs !! (length xs - 1)
+
+myInit1 :: [a] -> [a]
+myInit1 xs = take (length xs - 1) xs
+
+myInit2 :: [a] -> [a]
+myInit2 xs = reverse (tail (reverse xs))
+
